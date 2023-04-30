09e214855f98106679b0edee742233a9945aa5ab
Behrouz Eslami <eslamibehrouz17@gmail.com>
2023-05-01 01:26:20 +0330

    feat: main layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="py-4 bg-white shadow">
+        <div class="container mx-auto flex items-center">
+            <div class="font-semibold">Where in the world?</div>
+            <div class="flex-auto"></div>
+            <div>dark mode toggle</div>
+        </div>
+    </header>
+
+    <!-- page content -->
+    <div class="container mx-auto">
+        <slot />
+    </div>
+</template>
