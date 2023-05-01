6d846454880dbf2db752c2bc7fbff2ca9f88a94a
Behrouz Eslami <eslamibehrouz17@gmail.com>
2023-05-02 00:16:18 +0330

    feat: filter region menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,58 @@
 <template>
-  <div class="mb-6">
-    <div class="relative">
+  <div class="mb-6 flex">
+    <div class="relative w-1/3">
       <input
         type="text"
         placeholder="Search for a country..."
-        class="py-3 pl-12 rounded border-none dark:bg-neutral-800 w-1/3 shadow-md focus:ring-2 focus:ring-blue-800"
+        class="py-3 pl-12 rounded border-none dark:bg-neutral-800 w-full shadow-md focus:ring-2 focus:ring-blue-800"
       />
       <MagnifyingGlassIcon
         class="w-6 h-6 absolute left-3 top-3 text-neutral-500"
       />
     </div>
+    <div class="flex-auto"></div>
+    <Menu as="div" class="relative">
+      <div>
+        <MenuButton
+          class="inline-flex w-44 justify-center gap-x-1.5 rounded-md bg-white dark:text-white dark:bg-neutral-800 px-3 py-4 text-sm shadow-md hover:bg-gray-50"
+        >
+          Filter by region
+          <ChevronDownIcon
+            class="-mr-2 ml-4 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
+        </MenuButton>
+      </div>
+
+      <transition
+        enter-active-class="transition ease-out duration-100"
+        enter-from-class="transform opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-75"
+        leave-from-class="transform opacity-100 scale-100"
+        leave-to-class="transform opacity-0 scale-95"
+      >
+        <MenuItems
+          class="absolute right-0 z-10 mt-2 w-44 origin-top-right rounded-md bg-white dark:bg-neutral-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        >
+          <div class="py-1">
+            <MenuItem v-for="item in menuItems" v-slot="{ active }" :key="item">
+              <a
+                href="#"
+                :class="[
+                  active
+                    ? 'bg-neutral-100 dark:bg-neutral-700'
+                    : 'text-neutral-700 dark:text-white',
+                  'block px-4 py-2 text-sm'
+                ]"
+              >
+                {{ item }}
+              </a>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </transition>
+    </Menu>
   </div>
   <div class="py-8 grid grid-cols-4 gap-[74px]">
     <CountryCard
@@ -21,8 +64,11 @@
 </template>
 
 <script lang="ts" setup>
-import { MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
+import { MagnifyingGlassIcon, ChevronDownIcon } from '@heroicons/vue/20/solid'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { Country } from '@/interfaces/country'
+
+const menuItems = ['Africa', 'America', 'Asia', 'Europe', 'Oceania']
 
 const { data: countries } = await useFetch<Country[]>(
   'https://restcountries.com/v3.1/all',
